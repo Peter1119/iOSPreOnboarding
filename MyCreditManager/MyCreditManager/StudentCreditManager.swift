0707dbad653a14266ccc72b0395b040cb11e1b7e
Peter <ghdtjrgus333@naver.com>
2023-04-29 15:57:52 +0900

    feat. 학생 성적관리 매니저 및 프로토콜 사용

--- MyCreditManager/MyCreditManager/StudentCreditManager.swift
+++ MyCreditManager/MyCreditManager/StudentCreditManager.swift
@@ -1,5 +1,5 @@
 //
-//  StudentScoreManager.swift
+//  StudentCreditManager.swift
 //  MyCreditManager
 //
 //  Created by Sh Hong on 2023/04/29.
@@ -7,10 +7,62 @@
 
 import Foundation
 
-protocol StudentScoreManagerProtocol {
-    
+protocol StudentCreditManagerProtocol {
+    func addStudent(_ name: String) throws
+    func removeStudent(_ name: String) throws
+    func addGrade(_ name: String, subject: Subject) throws
+    func removeGrade(_ name: String, subject: Subject) throws
+    func getAverageCredit(_ name: String) throws
 }
 
-class StudentScoreManager {
+class StudentCreditManager: StudentCreditManagerProtocol {
+    private var students: [Student] = []
     
+    func addStudent(_ name: String) throws {
+        try validateName(name)
+        
+        let newStudent = Student(name: name)
+        students.append(newStudent)
+    }
+    
+    func removeStudent(_ name: String) throws {
+        try validateName(name)
+        
+        if students.map(\.name).contains(name) {
+            students.removeAll {
+                $0.name == name
+            }
+        } else {
+            throw InputError.existingStudent
+        }
+    }
+    
+    func addGrade(_ name: String, subject: Subject) throws {
+        try validateName(name)
+        
+        let student = students.first { $0.name == name }
+        
+        guard let student else {
+            throw InputError.invalidStudentName
+        }
+        
+        if student.subjects.map(\.name).contains(subject.name) {
+            
+        }
+
+    }
+    
+    func removeGrade(_ name: String, subject: Subject) throws {
+        try validateName(name)
+    }
+    
+    func getAverageCredit(_ name: String) throws {
+        try validateName(name)
+    }
+    
+    private func validateName(_ name: String) throws {
+        guard !(name.contains(" ") || name.isEmpty) else {
+            throw InputError.wrongValue
+        }
+    }
 }
