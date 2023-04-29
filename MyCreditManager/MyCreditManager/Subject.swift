13ca3471a8103520bffade637a03ee5fdaf915f2
Peter <ghdtjrgus333@naver.com>
2023-04-29 15:57:34 +0900

    chor. 학점 점수로 변환하는 함수 타입 메서드로 변경

--- MyCreditManager/MyCreditManager/Subject.swift
+++ MyCreditManager/MyCreditManager/Subject.swift
@@ -15,19 +15,19 @@ struct Subject {
         self.score = score
         self.name = name
     }
-}
-
-func changeCreditToScore(credit: String) -> Double? {
-    switch credit {
-    case "A+": return 4.5
-    case "A0": return 4.0
-    case "B+": return 3.5
-    case "B0": return 3.0
-    case "C+": return 2.5
-    case "C0": return 2.0
-    case "D+": return 1.5
-    case "D0": return 1.0
-    case "F": return 0.0
-    default: return nil
+    
+    static func changeCreditToScore(credit: String) -> Double? {
+        switch credit {
+        case "A+": return 4.5
+        case "A": return 4.0
+        case "B+": return 3.5
+        case "B": return 3.0
+        case "C+": return 2.5
+        case "C": return 2.0
+        case "D+": return 1.5
+        case "D": return 1.0
+        case "F": return 0.0
+        default: return nil
+        }
     }
 }
