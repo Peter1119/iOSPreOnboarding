8bb12c89fc2d97a8190e95025a6d01d2fbb39c14
Peter <ghdtjrgus333@naver.com>
2023-05-02 15:03:32 +0900

    feat. student equatable 추가

--- MyCreditManager/MyCreditManager/Student.swift
+++ MyCreditManager/MyCreditManager/Student.swift
@@ -7,11 +7,17 @@
 
 import Foundation
 
-class Student {
+class Student: Equatable {
     var name: String
     var subjects: [Subject] = []
     
     init(name: String) {
         self.name = name
+    }
+}
+
+extension Student {
+    static func == (lhs: Student, rhs: Student) -> Bool {
+        return lhs.name == rhs.name
     }
 }
